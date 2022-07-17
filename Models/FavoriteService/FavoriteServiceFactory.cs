c8d06672f9a767f4a78b21d21f2d5bc9f06eb29b
Discut <siiiro@outlook.com>
2022-07-18 02:57:24 +0800

    feat(favorite):收藏夹服务的实现 未完成

---
+++ Models/FavoriteService/FavoriteServiceFactory.cs
@@ -0,0 +1,23 @@
+﻿using MyWareHouse.Models.Data;
+using MyWareHouse.Models.FavoriteService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWareHouse.Models.FavoriteService
+{
+    static class FavoriteServiceFactory
+    {
+        public static IFavoriteGetter<Favorite> Getter()
+        {
+            return new Implement.FavoriteService();
+        }
+
+        public static IFavoriteSetter<Favorite> Setter()
+        {
+            return new Implement.FavoriteService();
+        }
+    }
+}
---
+++ Models/FavoriteService/Implement/FavoriteService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyWareHouse.Models.Data;
+using MyWareHouse.Models.Data.Factory;
+using MyWareHouse.Models.FavoriteService;
+
+namespace MyWareHouse.Models.FavoriteService.Implement
+{
+    /// <summary>
+    /// 收藏夹实现类
+    /// </summary>
+    class FavoriteService : IFavoriteGetter<Favorite>, IFavoriteSetter<Favorite>
+    {
+        public Favorite AddFavorite(string title = "未分类")
+        {
+            DataAccessLibrary.DataBaseSetter.IFavoriteSetter setter = DataAccessLibrary.DataBaseFactory.GetInstance().GetFavoriteSetter();
+            Dictionary<string, object> info = new Dictionary<string, object>();
+            info.Add("name", title);
+            IDictionary<string, object> newFavorite = setter.InsertFavorite(info);
+            return FavoriteFactory.GetFavorite(newFavorite);
+        }
+
+        public IList<Favorite> GetAllFavorites()
+        {
+            // 创建返回对象
+            List<Favorite> result = new List<Favorite>();
+            DataAccessLibrary.DataBaseGetter.IFavoriteGetter getter = DataAccessLibrary.DataBaseFactory.GetInstance().GetFavoriteGetter();
+
+            IList<IDictionary<string, object>> lists = getter.GetAllFavorites();
+            foreach(IDictionary < string, object> dic in lists)
+            {
+                result.Add(Data.Factory.FavoriteFactory.GetFavorite(dic));
+            }
+            return result;
+        }
+    }
+}
