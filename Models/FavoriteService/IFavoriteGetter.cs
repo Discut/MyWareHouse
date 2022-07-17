92b108fc0873f8be03068d469f9148dd4ffbc143
Discut <siiiro@outlook.com>
2022-07-18 02:56:44 +0800

    feat(IFavorite):收藏夹服务的接口抽象

---
+++ Models/FavoriteService/IFavoriteGetter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWareHouse.Models.FavoriteService
+{
+    /// <summary>
+    /// 收藏夹获取器
+    /// </summary>
+    interface IFavoriteGetter<T>
+    {
+        /// <summary>
+        /// 获取所有的收藏夹
+        /// </summary>
+        /// <returns>收藏夹列表</returns>
+        IList<T> GetAllFavorites();
+    }
+}
---
+++ Models/FavoriteService/IFavoriteSetter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWareHouse.Models.FavoriteService
+{
+    interface IFavoriteSetter<T>
+    {
+        /// <summary>
+        /// 添加新的收藏夹
+        /// </summary>
+        /// <param name="title">收藏夹名称</param>
+        /// <returns>返回插入的收藏夹</returns>
+        T AddFavorite(string title = "未分类");
+    }
+}
