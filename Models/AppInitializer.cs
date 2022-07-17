eecf5246ed4f2b9d87d42371701b37be658ad367
Discut <siiiro@outlook.com>
2022-07-18 03:01:20 +0800

    feat(init):初始化器与初始化器的接口

---
+++ Models/AppInitializer.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLibrary;
+using MyWareHouse.Models.FileService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWareHouse.Models
+{
+    class AppInitializer : IAppInitializer
+    {
+        private static class Inner
+        {
+            public static IAppInitializer initializer = new AppInitializer();
+        }
+        public static IAppInitializer Instance
+        {
+            get
+            {
+                return Inner.initializer;
+            }
+        }
+        public async Task Init()
+        {
+            // 初始化数据库
+            IDataBaseFactory dataBaseFactory = DataBaseFactory.GetInstance();
+            await dataBaseFactory.Init();
+            // 初始文件服务与化对象池
+            await ImageFileService.Instance().InitAsync();
+        }
+    }
+}
